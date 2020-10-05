b86b9370aed7d5ed6cf1e44d7aa590e90c0fe2f2
Dawid <dawid.kaluzny@empik.com>
2020-10-05 20:11:22 +0200

    Inittial second fragment

---
+++ app/src/main/java/com/example/stretching/featurewelcome/WelcomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.stretching.featurewelcome
+
+import androidx.fragment.app.Fragment
+import com.example.stretching.R
+
+class WelcomeFragment  : Fragment(R.layout.fragment_welcome) {
+}
